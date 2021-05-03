b245d2d5b445f1d2d6fead1399df0910764dbd6c
Sean Knowles <s@seanknowles.dev>
2021-05-03 10:16:01 +0100

    added default port again

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 import userRoutes from "./components/users/userRoutes.js";
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 // cors
 app.use(cors())
